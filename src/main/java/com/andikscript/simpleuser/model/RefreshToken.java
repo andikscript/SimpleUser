eac41f3e767a1adf64cb31ce8b19ec00f5820a8f
andikscript <andikscript@gmail.com>
2022-07-12 20:05:21 +0700

    add model

---
+++ src/main/java/com/andikscript/simpleuser/model/RefreshToken.java
@@ -0,0 +1,59 @@
+package com.andikscript.simpleuser.model;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "refreshtoken")
+public class RefreshToken {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "id_user", nullable = false)
+    private User idUser;
+
+    @Column(name = "token", nullable = false, length = 1024)
+    private String token;
+
+    @Column(name = "expired_date", nullable = false)
+    private Instant expiredDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(User idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Instant getExpiredDate() {
+        return expiredDate;
+    }
+
+    public void setExpiredDate(Instant expiredDate) {
+        this.expiredDate = expiredDate;
+    }
+
+}
---
+++ src/main/java/com/andikscript/simpleuser/model/Role.java
@@ -0,0 +1,34 @@
+package com.andikscript.simpleuser.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "id_roles", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
