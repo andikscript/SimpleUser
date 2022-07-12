15e552e059172d53f10e53ce04535b5502677d36
andikscript <andikscript@gmail.com>
2022-07-12 21:40:10 +0700

    add sign in and refresh token controller

--- src/main/java/com/andikscript/simpleuser/controller/AuthController.java
+++ src/main/java/com/andikscript/simpleuser/controller/AuthController.java
@@ -1,14 +1,28 @@
 package com.andikscript.simpleuser.controller;
 
+import com.andikscript.simpleuser.exception.TokenRefreshException;
+import com.andikscript.simpleuser.model.RefreshToken;
 import com.andikscript.simpleuser.model.User;
+import com.andikscript.simpleuser.payload.JwtResponse;
+import com.andikscript.simpleuser.payload.TokenRefreshRequest;
+import com.andikscript.simpleuser.payload.TokenRefreshResponse;
+import com.andikscript.simpleuser.payload.UsernamePasswordRequest;
 import com.andikscript.simpleuser.repository.UserRepository;
+import com.andikscript.simpleuser.security.jwt.JwtUtils;
+import com.andikscript.simpleuser.security.jwt.RefreshTokenService;
+import com.andikscript.simpleuser.security.service.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -20,6 +34,15 @@ public class AuthController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
 
     @PostMapping(value = "/signup", consumes = "application/json")
     public ResponseEntity addUser(@RequestBody User user) {
@@ -34,8 +57,37 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping(value = "/all")
-    public List<User> findAll() {
-        return userRepository.findAll();
+    @PostMapping(value = "/signin", consumes = "application/json")
+    public ResponseEntity<JwtResponse> authenticateUser(@RequestBody UsernamePasswordRequest usernamePasswordRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usernamePasswordRequest.getUsername(),
+                        usernamePasswordRequest.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        String jwt = jwtUtils.generateJwtToken(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getId());
+        return ResponseEntity.ok(new JwtResponse(jwt, refreshToken.getToken(),
+                userDetails.getUsername(),
+                userDetails.getEmail(), roles));
+    }
+
+    @PostMapping(value = "/refreshtoken", consumes = "application/json")
+    public ResponseEntity<TokenRefreshResponse> refreshToken(@RequestBody TokenRefreshRequest tokenRefreshRequest) {
+        String requestRefreshToken = tokenRefreshRequest.getRefreshToken();
+
+        return refreshTokenService.findByToken(requestRefreshToken)
+                .map(refreshTokenService::verifyExpiration)
+                .map(user -> {
+                    User user1 = user.getIdUser();
+                    String token = jwtUtils.generateTokenFromUsername(user1.getUsername());
+                    return ResponseEntity.ok(new TokenRefreshResponse(token, requestRefreshToken));
+                })
+                .orElseThrow(() -> new TokenRefreshException(requestRefreshToken, "Refresh token not store on database"));
     }
 }
