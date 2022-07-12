f207d528bcb099c0933cb0e04a43eaa85b0f4840
andikscript <andikscript@gmail.com>
2022-07-12 21:37:52 +0700

    add Jwt response

---
+++ src/main/java/com/andikscript/simpleuser/payload/JwtResponse.java
@@ -0,0 +1,44 @@
+package com.andikscript.simpleuser.payload;
+
+import java.util.List;
+
+public class JwtResponse {
+    private String accessToken;
+    private String type = "Bearer";
+    private String refreshToken;
+    private String username;
+    private String email;
+    private List roles;
+
+    public JwtResponse(String accessToken, String refreshToken, String username, String email, List roles) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List getRoles() {
+        return roles;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+}
