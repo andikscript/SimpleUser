980536ed538cd0950dd7962964588a6f0c174d4e
andikscript <andikscript@gmail.com>
2022-07-14 16:42:56 +0700

    add annotation repository

--- src/main/java/com/andikscript/simpleuser/repository/RefreshTokenRepository.java
+++ src/main/java/com/andikscript/simpleuser/repository/RefreshTokenRepository.java
@@ -2,10 +2,11 @@ package com.andikscript.simpleuser.repository;
 
 import com.andikscript.simpleuser.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
-
     Optional<RefreshToken> findByToken(String token);
 }
--- src/main/java/com/andikscript/simpleuser/repository/UserRepository.java
+++ src/main/java/com/andikscript/simpleuser/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.andikscript.simpleuser.repository;
 
 import com.andikscript.simpleuser.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 }
