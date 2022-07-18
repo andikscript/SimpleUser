f72d66e63dbb32fe46ea4ee56deb272e0824f647
andikscript <andikscript@gmail.com>
2022-07-18 19:15:13 +0700

    add limit for public, user, moderator, admin and root

--- src/main/java/com/andikscript/simpleuser/controller/TestController.java
+++ src/main/java/com/andikscript/simpleuser/controller/TestController.java
@@ -9,30 +9,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/test")
 @EnableCaching // mengaktifkan fitur caching untuk menyimpan request untuk konfigurasi rate limit
 public class TestController {
-
-    @GetMapping(value = "/all")
+    @GetMapping(value = {"/all", "al"})
     public String allAccess() {
         return "Public Contenttt";
     }
-
     @GetMapping(value = "/user")
     @PreAuthorize("hasRole('USER')")
     public String userAccess() {
         return "User Content";
     }
-
     @GetMapping(value = "/mod")
     @PreAuthorize("hasRole('MODERATOR')")
     public String moderatorAccess() {
         return "Moderator Content";
     }
-
     @GetMapping(value = "/admin")
     @PreAuthorize("hasRole('ADMIN')")
     public String adminAccess() {
         return "Admin Content";
     }
-
     @GetMapping(value = "/root")
     @PreAuthorize("hasRole('ROOT')")
     public String rootAccess() {
