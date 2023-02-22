fb8c19277596a85adc6a3fdfa34a4f4a2b959331
andik.saputro <andik.saputro@pactindo.com>
2023-02-22 13:12:42 +0700

    add config for running .war

--- src/main/java/com/andikscript/simpleuser/SimpleUserApplication.java
+++ src/main/java/com/andikscript/simpleuser/SimpleUserApplication.java
@@ -2,9 +2,16 @@ package com.andikscript.simpleuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimpleUserApplication {
+public class SimpleUserApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SimpleUserApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleUserApplication.class, args);
